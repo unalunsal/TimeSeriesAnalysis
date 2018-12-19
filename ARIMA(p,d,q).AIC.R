7a9dc47bd07e9d08387dc04b235b0e10461977c7
quicktur <12380228+unalunsal@users.noreply.github.com>
2018-12-19 11:31:39 -0800

    Update ARIMA(p,d,q).AIC.R

--- ARIMA(p,d,q).AIC.R
+++ ARIMA(p,d,q).AIC.R
@@ -56,7 +56,7 @@ recursive <- function(SYMBOL, SOURCE,testRatio, infoCrea) {
   return(rmse[2:length(rmse)])
 }
 
-rmse = recursive("DEXUSEU", "FRED", 0.999, AIC)
+rmse = recursive("DEXUSEU", "FRED", 0.5, AIC)
 
 rmse = sqrt ( mean( rmse ^ 2 ) )
 
